da2bed7f867ea97a95fa42a347fcf1951c21488b
hyuk95 <lumin4546@naver.com>
2023-11-08 19:09:07 +0900

    상혁서비스 수정

--- workspace/SlimBear/src/main/java/com/kgitbank/slimbear/service/SangyhyukServiceImpl.java
+++ workspace/SlimBear/src/main/java/com/kgitbank/slimbear/service/SangyhyukServiceImpl.java
@@ -12,9 +12,6 @@ import com.kgitbank.slimbear.dto.ReviewDTO;
 @Service
 public class SangyhyukServiceImpl {
 	
-	@Autowired
-	private MemberDAO memDAO;
-	
 	public List<ReviewDTO> getReview(){
 		
 		List<ReviewDTO> list = new ArrayList<ReviewDTO>();
