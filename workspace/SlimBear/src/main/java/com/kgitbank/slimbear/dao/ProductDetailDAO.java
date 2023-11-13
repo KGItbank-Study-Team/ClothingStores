6be8e017a452822d71c47c7f58716522c8c99d41
smsy112 <smsy112@naver.com>
2023-11-13 13:53:02 +0900

    2023-11-13(승연) ProductDetailDAO, CategoryDTO, product-mapping.xml (수정)

--- workspace/SlimBear/src/main/java/com/kgitbank/slimbear/dao/ProductDetailDAO.java
+++ workspace/SlimBear/src/main/java/com/kgitbank/slimbear/dao/ProductDetailDAO.java
@@ -1,13 +1,43 @@
 package com.kgitbank.slimbear.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.kgitbank.slimbear.dto.ProductDTO;
+import com.kgitbank.slimbear.dto.ProductDetailDTO;
+
 @Repository
 public class ProductDetailDAO {
-	
+
 	@Autowired
 	protected SqlSessionTemplate template;
 
+	public List<ProductDetailDTO> getProductDetailList(long uid) {
+		ProductDTO info = new ProductDTO();
+		info.setUid(uid);
+		return template.selectList("com.slimbear.mapper.Product.SELECT_PRODUCT_DETAIL_LIST", info);
+	}
+
+	public ProductDetailDTO getProductDetail(long prod_uid, String color, String size) {
+		ProductDetailDTO info = new ProductDetailDTO();
+		info.setProd_uid(prod_uid);
+		info.setColor(color);
+		info.setSize(size);
+		return template.selectOne("com.slimbear.mapper.Product.SELECT_PRODUCT_DETAIL", info);
+	}
+	
+	public int addProductDetail(ProductDetailDTO productDetail) {		
+		return template.insert("com.slimbear.mapper.Product.INSERT_PRODUCT_DETAIL", productDetail);
+	}
+	
+	public int updateProductDetail(ProductDetailDTO productDetail) {
+		return template.update("com.slimbear.mapper.Product.UPDATE_PRODUCT_DETAIL", productDetail);
+	}
+	
+	public int deleteProductDetail(ProductDetailDTO productDetail) {
+		return template.delete("com.slimbear.mapper.Product.DELETE_PRODUCT_DETAIL", productDetail);
+	}
 }
