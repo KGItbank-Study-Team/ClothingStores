f087d289e6002ff434b5f85361c9890373e14803
DESKTOP-T87KHS2\user <ydw5112@naver.com>
2023-11-16 09:51:09 +0900

    Faq 수정

--- workspace/SlimBear/src/main/java/com/kgitbank/slimbear/dao/FaqDAO.java
+++ workspace/SlimBear/src/main/java/com/kgitbank/slimbear/dao/FaqDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
 import com.kgitbank.slimbear.dto.FaqDTO;
 
 
@@ -18,6 +19,12 @@ public class FaqDAO {
 	public List<FaqDTO> getFaqList() { 
 		return template.selectList("com.slimbear.mapper.Board.SELECT_FAQ_LIST"); // 패키지풀네임.id
 	}
+	// 특정 type으로 필터링된 상품문의 리스트 조회
+    public List<FaqDTO> getFaqListByType(String type) { 
+    	FaqDTO Faq = new FaqDTO();
+		Faq.setType(type);
+        return template.selectList("com.slimbear.mapper.Board.SELECT_FAQ_LIST_BY_TYPE", Faq);
+    }
 
 	// FAQ 데이터 삽입
 	public int insertFaq(FaqDTO faq) {
