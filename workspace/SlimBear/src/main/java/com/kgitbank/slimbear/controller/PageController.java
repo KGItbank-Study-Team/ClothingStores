b48f07b699426f259074d9a3ff768fb8a64f421b
milgos <milgos@naver.com>
2023-11-20 13:57:30 +0900

    관리자 로그인시 관리자 페이지 이동 처리 및 thymeleaf security

--- workspace/SlimBear/src/main/java/com/kgitbank/slimbear/controller/PageController.java
+++ workspace/SlimBear/src/main/java/com/kgitbank/slimbear/controller/PageController.java
@@ -1,9 +1,10 @@
 package com.kgitbank.slimbear.controller;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,29 +18,39 @@ public class PageController {
 
 	@Autowired
 	private PageConfigServiceImpl pageConfigService;
-	
+
 	@Autowired
 	private ProductServiceImpl productService;
 
 	@RequestMapping("main")
-	public String mainePage(HttpSession session, Model model) {
+	public String mainePage(Model model) {
 
 		model.addAttribute("productBannerTop", pageConfigService.getProductBannerTop());
 		model.addAttribute("hotProductList", productService.getHotProductList());
-		
+
 		return "main";
 	}
 
 	@RequestMapping("login")
 	public String loginPage(@RequestParam(required = false) String error, HttpServletRequest request) {
-		
-		if(error != null) {
+
+		if (error != null) {
 			request.setAttribute("error", "error");
 		}
-		
+
 		return "login";
 	}
-	
+
+	@RequestMapping("login/success")
+	public String loginSuccessPage(Authentication authentication, HttpServletRequest request) {
+
+		if (authentication.getAuthorities().contains(AuthorityUtils.createAuthorityList("ADMIN").get(0))) {
+			return "redirect:/admin/home";
+		}else {
+			return "redirect:/";
+		}
+	}
+
 	@RequestMapping("join")
 	public String joinPage(HttpServletRequest request) {
 		return "join";
--- workspace/SlimBear/src/main/java/com/kgitbank/slimbear/security/SecurityConfig.java
+++ workspace/SlimBear/src/main/java/com/kgitbank/slimbear/security/SecurityConfig.java
@@ -25,16 +25,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		// 로그인해야만 갈수있는 화면
 		security.authorizeRequests().antMatchers("/app/member/myPage/**").authenticated();
-		//security.authorizeRequests().antMatchers("/admin/home/**").authenticated();
+		security.authorizeRequests().antMatchers("/admin/home/**").authenticated();
 		
 		// 로그인한 사용자가 해당 권한일경우 사용가능
 		security.authorizeRequests().antMatchers("/app/member/myPage/**").hasAnyAuthority("MEMBER");
-		//security.authorizeRequests().antMatchers("/admin/home/**").hasAnyAuthority("ADMIN");
+		security.authorizeRequests().antMatchers("/admin/home/**").hasAnyAuthority("ADMIN");
 		
 		security.csrf().disable();
 		
 		security.formLogin().loginPage("/app/login").loginProcessingUrl("/app/member/login")
-		.defaultSuccessUrl("/").failureForwardUrl("/app/login?error");
+		.defaultSuccessUrl("/app/login/success").failureForwardUrl("/app/login?error");
 		security.logout().logoutUrl("/app/member/logout").invalidateHttpSession(true);// .deleteCookies(); 쿠키까지 하고싶으면 사용
 		
 		// 권한 없을 경우
