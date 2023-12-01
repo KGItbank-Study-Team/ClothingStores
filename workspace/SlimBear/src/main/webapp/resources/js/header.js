f850e4bdd22223d84963a3af6c9579dc81920a85
hyuk95 <lumin4546@naver.com>
2023-12-01 10:57:46 +0900

    헤더 수정

--- workspace/SlimBear/src/main/webapp/resources/js/header.js
+++ workspace/SlimBear/src/main/webapp/resources/js/header.js
@@ -1,11 +1,11 @@
 //상하슬라이딩
 setInterval(function(){
-  $('#oneTop>ul>li').delay(2500);
-  $('#oneTop>ul>li').animate({marginTop: "-39px"})
-  $('#oneTop>ul>li').delay(2500);
-  $('#oneTop>ul>li').animate({marginTop: "-51px"})
-  $('#oneTop>ul>li').delay(2500);
-  $('#oneTop>ul>li').animate({marginTop: "0px"})
+  $('#oneTop1>ul>li').delay(2500);
+  $('#oneTop1>ul>li').animate({marginTop: "-39px"})
+  $('#oneTop1>ul>li').delay(2500);
+  $('#oneTop1>ul>li').animate({marginTop: "-51px"})
+  $('#oneTop1>ul>li').delay(2500);
+  $('#oneTop1>ul>li').animate({marginTop: "0px"})
 });
 
 // 스크롤시 메뉴바 색깔 변화
